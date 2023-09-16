36e1d8b0f4dbf6f400472766cfb2ae09d027b68f
phaze-the-dumb <phaze@duck.com>
2023-09-16 13:29:57 +0100

    update shtuff

--- src/editor/class/WindowControllers/InspectorWindowController.ts
+++ src/editor/class/WindowControllers/InspectorWindowController.ts
@@ -4,22 +4,35 @@ import { WindowController } from "../WindowController";
 import { GameObject } from "../../../felix/class/GameObject";
 import { Component } from "../../../felix/class/GameObject/Component";
 
+let colours: any = {
+  'Camera': '#c5ffc3'
+};
+
 class InspectorComponent{
   div: HTMLElement;
   component: Component;
+  content: HTMLElement;
 
   constructor( component: Component ){
     this.div = document.createElement("div");
     this.component = component;
 
     this.div.classList.add("inspector-component");
-    this.div.style.borderLeft = this.component.colour + ' 2px solid';
+    this.div.style.borderLeft = (colours[this.component.name] || '#ffc3c3') + ' 2px solid';
 
     let header = document.createElement("div");
     header.classList.add("inspector-component-header");
     header.innerHTML = component.name;
 
+    this.content = document.createElement("div");
+    this.content.style.textAlign = 'left';
+    this.content.style.marginLeft = '5px';
+    this.content.innerHTML = 'fuck';
+
     this.div.appendChild(header);
+    this.div.appendChild(this.content);
+
+    console.log(Object.keys(component.definePublicValues));
   }
 }
 
